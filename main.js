266cc45336eada77b543ad81b254d869876302e4
Siddhartha Konduru <kondurus@usf.edu>
2024-12-17 10:06:37 -0500

    Date Itemization

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+// Current Date Display
 const currentDate = new Date();
-console.log("Current Date and Time:", currentDate.toString());
+console.log("Current Date:", currentDate.toString());
+
+// Current Date Sections Displayed
+console.log("Date Indexed down by Year, Month, and Day")
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth();  
+const day = currentDate.getDate();
+const dayOfWeek = currentDate.getDay();  
